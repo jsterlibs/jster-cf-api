c2f53553b1f714f280b7019a41b03b161a2280ad
Juho Vepsäläinen <bebraw@gmail.com>
2022-04-11 11:15:20 +0300

    feat: Fetch data from gh api

--- workers/stargazers.ts
+++ workers/stargazers.ts
@@ -1,12 +1,23 @@
 export default {
-  fetch(
+  async fetch(
     request: Request,
     // env: { cfAccountId: string; cfApiToken: string; apiSecret: string },
   ) {
     const { searchParams } = new URL(request.url);
-    const repo = searchParams.get('repo')
+    const organization = searchParams.get("organization");
+    const repository = searchParams.get("repository");
 
-    if (!repo) {
+    if (!organization) {
+      return new Response(
+        `{ "error": "Missing organization query parameter" }`,
+        {
+          status: 500,
+          headers: { "content-type": "application/json" },
+        },
+      );
+    }
+
+    if (!repository) {
       return new Response(`{ "error": "Missing repository query parameter" }`, {
         status: 500,
         headers: { "content-type": "application/json" },
@@ -14,13 +25,24 @@ export default {
     }
 
     // TODO: Add token in front (see survivejs api)
-    // TODO: Get data from gh api
     // TODO: Use auth with gh api to increase api limit
     // TODO: Cache responses using kv (cache for a day)
 
     // Example
     // https://api.github.com/repos/plotly/dash
+    const response = await fetch(
+      `https://api.github.com/repos/${organization}/${repository}`,
+    );
+    const { stargazers_count: stargazers } = await response.json();
 
-    return new Response(repo);
+    return new Response(
+      JSON.stringify({ stargazers }),
+      {
+        status: 200,
+        headers: {
+          "content-type": "application/json",
+        },
+      },
+    );
   },
 };
