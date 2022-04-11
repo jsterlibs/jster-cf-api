a91599ce6a1773a9baafbe87b9f5fb5725a13224
Juho Vepsäläinen <bebraw@gmail.com>
2022-04-11 10:57:43 +0300

    feat: Parse query parameter

--- workers/stargazers.ts
+++ workers/stargazers.ts
@@ -1,12 +1,26 @@
 export default {
   fetch(
-    // request: Request,
+    request: Request,
     // env: { cfAccountId: string; cfApiToken: string; apiSecret: string },
   ) {
+    const { searchParams } = new URL(request.url);
+    const repo = searchParams.get('repo')
+
+    if (!repo) {
+      return new Response(`{ "error": "Missing repository query parameter" }`, {
+        status: 500,
+        headers: { "content-type": "application/json" },
+      });
+    }
+
     // TODO: Add token in front (see survivejs api)
     // TODO: Get data from gh api
     // TODO: Use auth with gh api to increase api limit
     // TODO: Cache responses using kv (cache for a day)
-    return new Response('stargazers');
+
+    // Example
+    // https://api.github.com/repos/plotly/dash
+
+    return new Response(repo);
   },
 };
